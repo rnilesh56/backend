5d8c6ed67f5fd1a008472433bf804552d7403ad4
Nilesh Raj <rnilesh56@gmail.com>
2023-12-30 13:36:50 +0530

    typo in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,7 +36,7 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password".post(verifyJWT, changeCurrentPassword));
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-accpunt").patch(verifyJWT, updateAccountDetails);
 router
@@ -44,7 +44,7 @@ router
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
